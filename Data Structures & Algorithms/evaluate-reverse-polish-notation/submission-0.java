bbb2b1b1d1a89b699b58ea3c25c3fa48b5c5c417
Yashodhan Reddy Bhimavarapu <162135730+2300033084@users.noreply.github.com>
2026-08-21 23:47:17 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+
+            if (!tokens[i].equals("+") &&
+                !tokens[i].equals("-") &&
+                !tokens[i].equals("*") &&
+                !tokens[i].equals("/")) {
+
+                st.push(Integer.parseInt(tokens[i]));
+            }
+            else {
+                int b = st.pop();
+                int a = st.pop();
+
+                if (tokens[i].equals("+")) {
+                    st.push(a + b);
+                }
+                else if (tokens[i].equals("-")) {
+                    st.push(a - b);
+                }
+                else if (tokens[i].equals("*")) {
+                    st.push(a * b);
+                }
+                else {
+                    st.push(a / b);
+                }
+            }
+        }
+
+        return st.pop();
+    }
+}
